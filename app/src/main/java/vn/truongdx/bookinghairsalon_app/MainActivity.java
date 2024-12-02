53e577d407b1fef48621b1e947f21d5cd2aa89e9
Xuân Trường <dxt8104@gmail.com>
2024-12-02 13:43:01 +0700

    push lần 5: viết hàm kết nối đến Realtime Database

--- app/src/main/java/vn/truongdx/bookinghairsalon_app/MainActivity.java
+++ app/src/main/java/vn/truongdx/bookinghairsalon_app/MainActivity.java
@@ -1,6 +1,9 @@
 package vn.truongdx.bookinghairsalon_app;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +12,31 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    //khai báo biến
+    EditText tendn, mathkhau;
+    Button dangnhap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        tendn = findViewById(R.id.txt_tendn);
+        mathkhau = findViewById(R.id.txt_mk);
+        dangnhap = findViewById(R.id.btn_dangnhap);
+
+        //lắng nghe sự kiện nút đăng nhập
+        dangnhap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NextPage_Home(view);
+            }
         });
+    }
+
+    //hàm thực hiện chương trình
+    public void NextPage_Home(View view) {
+
     }
 }
--- app/src/main/java/vn/truongdx/bookinghairsalon_app/utils/DatabaseConnection.java
+++ app/src/main/java/vn/truongdx/bookinghairsalon_app/utils/DatabaseConnection.java
@@ -1,4 +1,15 @@
 package vn.truongdx.bookinghairsalon_app.utils;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class DatabaseConnection {
+    //tham chiếu đến FirebaseDatabase
+    public static FirebaseDatabase getDatabaseInstance() {
+        return FirebaseDatabase.getInstance();
+    }
+
+    //tham chiếu đến một node
+    public static DatabaseReference getDatabaseReference(String nodeName) {
+        return getDatabaseInstance().getReference(nodeName);
+    }
 }
