fa485284e001b0bed7cb4500938b65e84b3323d8
Xuân Trường <dxt8104@gmail.com>
2024-12-03 11:13:32 +0700

    push lần 17: viết hàm thực hiện đặt lịch ở fragment booking

--- app/src/main/java/vn/truongdx/bookinghairsalon_app/fragments/Booking_Fragment.java
+++ app/src/main/java/vn/truongdx/bookinghairsalon_app/fragments/Booking_Fragment.java
@@ -1,5 +1,6 @@
 package vn.truongdx.bookinghairsalon_app.fragments;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,14 +8,25 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 import vn.truongdx.bookinghairsalon_app.R;
 
 public class Booking_Fragment extends Fragment {
-    
+    //khai báo biến
+    private CheckBox cbCat, cbUon, cbNhuom, cbDuoi, cbPhuchoi;
+    private CheckBox cbGoidauthg, cbGoidauds;
+    private CheckBox cbMakeup, cbNoimi;
+    private CheckBox cbLaymun, cbNail;
+    private Button btnSelectAll, btnCancel;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_booking, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_booking, container, false);
+        return view;
     }
 }
