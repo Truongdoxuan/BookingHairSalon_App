1d4449f3673bb225aef5f7c3f67f1731df14d9d3
Xuân Trường <dxt8104@gmail.com>
2024-12-09 23:35:35 +0700

    push lần 18: sửa lỗi logic

--- app/src/main/java/vn/truongdx/bookinghairsalon_app/fragments/Booking_Fragment.java
+++ app/src/main/java/vn/truongdx/bookinghairsalon_app/fragments/Booking_Fragment.java
@@ -1,6 +1,7 @@
 package vn.truongdx.bookinghairsalon_app.fragments;
 
 import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -11,22 +12,60 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import vn.truongdx.bookinghairsalon_app.R;
 
 public class Booking_Fragment extends Fragment {
     //khai báo biến
-    private CheckBox cbCat, cbUon, cbNhuom, cbDuoi, cbPhuchoi;
-    private CheckBox cbGoidauthg, cbGoidauds;
-    private CheckBox cbMakeup, cbNoimi;
-    private CheckBox cbLaymun, cbNail;
-    private Button btnSelectAll, btnCancel;
+    private Button btnchonDV;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_booking, container, false);
+
+        btnchonDV = view.findViewById(R.id.btn_select_dv);
+
+        //sự kiện khi onclick
+        btnchonDV.setOnClickListener(v -> showCheckbox());
+
         return view;
+    }
+    //hàm thực hiện chương trình
+    private void showCheckbox() {
+//        //gọi mảng danh sách dịch vụ trong string.xml
+//        final String[] listDv = getResources().getStringArray(R.array.sp_list);
+//        //tạo mảng lưu trạng trái checkbox
+//        final boolean[] selectedSP = new boolean[listDv.length];
+//
+//        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+//        builder.setMultiChoiceItems(listDv, selectedSP, new DialogInterface.OnMultiChoiceClickListener() {
+//            @Override
+//            public void onClick(DialogInterface dialogInterface, int i, boolean b) {
+//                //lưu trạng thái checkbox khi đc thay đổi
+//                selectedSP[which] = isChecked;
+//            }
+//        })
+//                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialogInterface, int i) {
+//                        StringBuilder selected = new StringBuilder();
+//                        for (int i = 0; i < listDv.length; i++) {
+//                            if (selectedSP[i]) {
+//                                selected.append(listDv[i]).append("\n");
+//                            }
+//                        }
+//                        if (selected.length() > 0) {
+//                            // Hiển thị các dịch vụ đã chọn
+//                            Toast.makeText(getActivity(), "Dịch vụ đã chọn: \n" + selected.toString(), Toast.LENGTH_LONG).show();
+//                        } else {
+//                            Toast.makeText(getActivity(), "Chưa chọn dịch vụ nào", Toast.LENGTH_SHORT).show();
+//                        }
+//                    }
+//                })
+//                .setNegativeButton("Hủy",null);
+//        builder.create().show();
     }
 }
